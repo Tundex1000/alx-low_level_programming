57f1ca5a163c48d71949f50db706a4f65e8d9d28
Omisore Tunde <omisore506@gmail.com>
2022-12-08 13:44:48 -0800

    hello out

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+
+}
