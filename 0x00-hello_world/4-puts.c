aeac941b382a836625197d9f9411b0f8b6b5246c
Omisore Tunde <omisore506@gmail.com>
2022-12-08 14:25:10 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Alway 0 (Success)
+ * main - print a line of code using puts
+ *
+ *Return: 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
-
+put("\"programming is like building a multilingual puzzle");
+return (0);
 }
