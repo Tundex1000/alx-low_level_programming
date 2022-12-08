39dffcda19586d4fc9a1b2b6bc3b722c9a634d27
Omisore Tunde <omisore506@gmail.com>
2022-12-08 14:59:34 -0800

    hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,9 @@
  *
  *Return: 0
  */
-int main(void)
+int
+main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
