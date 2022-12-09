b05733832037f8f7718787d03f563e4d872d9cc2
Omisore Tunde <omisore506@gmail.com>
2022-12-08 17:46:06 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - print exactly and that piece of art is useful
- *
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 1
  */
-
 int main(void)
 {
 	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
