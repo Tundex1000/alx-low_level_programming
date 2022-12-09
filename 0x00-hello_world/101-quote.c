5b84317537a048e2cbae615d808ffaea21f273c6
Omisore Tunde <omisore506@gmail.com>
2022-12-08 20:21:03 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 i#include <stdio.h>
-#;;;;;;;include <unistd.h>
+#include <unistd.h>
+
 /**
- * main - Entry point
- * Return: Always 1
+ * main - main function in the task
+ * return: return 1
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
 
-/**
- * main - This is the entry level of the project
- * This entry level is called main
- * Return: it has a return statement
- */
-
-int main(void)
-{
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return (0);
-}
