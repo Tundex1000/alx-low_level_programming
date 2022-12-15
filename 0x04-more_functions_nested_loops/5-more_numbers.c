0c412e4a69da61f8ecef144d2cd8a1a7cd0b2208
Omisore Tunde <omisore506@gmail.com>
2022-12-15 06:02:59 -0800

    number constitute the universe

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,32 @@
-#include “main.h”
-/**
-*more_numbers - prints numbers to 14
-*
-*Return: returns nothing
-*/
-void more numbers(void)
-{
-int number1, number2;
-for (number1 = 0; number1 < 10; number1++)
-{
-for (number2 = 0; number2 <= 14; number2++)
+#include "main.h"
 
+/**
+ * more_numbers - prints more numbers
+ * Return:void
+ */
+
+void more_numbers(void)
 {
-if (number2 > 9)
-{
-_putchar ((number2 / 10) + '0’);
-}
-_putchar ((number2 % 10) + ‘0’);
-}
-_putchar (10) ;
-}
+	char n, c;
+	int i = 0;
+
+
+	while (i < 10)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			c = n;
+			if (n > 9)
+			{
+				_putchar('1');
+				c = n % 10;
+			}
+
+			_putchar('0' + c);
+		}
+
+		_putchar('\n');
+		i++;
+	}
+
 }
