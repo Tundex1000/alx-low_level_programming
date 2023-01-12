a110b9f41ad689a08cc82d60440907d1eeb60ab9
Omisore Tunde <omisore506@gmail.com>
2023-01-12 10:33:28 -0800

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *malloc_checked - allocates memory using malloc
+ *
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
